fdf46441ceba114acbc0a251ff9dbfee2e95c391
MathiasGilles <mathiasgilles136@gmail.com>
2022-07-07 10:14:19 +0200

    Add name in main view

--- src/views/Dashboard/Main.vue
+++ src/views/Dashboard/Main.vue
@@ -1,7 +1,13 @@
 <template>
   <div
-      style="display: flex; justify-content: center; align-items: center; height: 80%; padding: 3rem; background: darkgrey">
+      style="display: flex; justify-content: center; align-items: center; height: 80%; padding: 3rem; background: darkgrey;flex-direction: column">
     <button @click="handleClick">Create analyse button</button>
+    <div style="display: flex;gap: 20px;">
+      <p>Mathias GILLES</p>
+      <p>Jules DAYAUX</p>
+      <p>Sosthène FRUCHARD</p>
+      <p>Yanny OUZID</p>
+    </div>
   </div>
 </template>
 
